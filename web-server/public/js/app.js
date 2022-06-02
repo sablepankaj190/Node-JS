8be5f4379cb4e40384cd9f14ebbe3305a5a2376f
Pankaj Sable <pansable@publicisgroupe.net>
2022-06-02 15:58:39 +0530

    Added About info

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,7 +4,7 @@ const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
-
+// https://sable-weather-application.herokuapp.com/
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
