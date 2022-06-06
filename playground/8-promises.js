44aec3805015d35f4fd9b5bf8da6052660abdffc
Pankaj <sablepankaj190@gmail.com>
2022-06-06 16:24:01 +0530

    Second commit for Weather App Web Server with integrated UI using HBS template engine

---
+++ playground/8-promises.js
@@ -0,0 +1,23 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+// add(12, 24).then((sum) => {
+//     console.log(sum);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+add(1, 1).then((sum) => {
+    console.log(sum)
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log(sum2)
+})
+
+
+
